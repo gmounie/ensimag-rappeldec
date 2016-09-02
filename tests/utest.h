98fba9a9630d891f900c1183385d396ac7c3f56f
Grégory Mounié <Gregory.Mounie@imag.fr>
2016-09-02 10:13:29 +0200

    date copyright et nom paquetage

--- tests/utest.h
+++ tests/utest.h
@@ -5,17 +5,17 @@
    Minimalist unit testing, very partially from an idea of
    http://www.jera.com/techinfo/jtns/jtn002.html
 
-   Copyright (C) 2015 by Gregory Mounie 
+   Copyright (C) 2015-2016 by Gregory Mounie 
    
-   This file is part of ExamGC
+   This file is part of RappelDeC
 
-   ExamGC is free software: you can redistribute it and/or modify it
+   RappelDeC is free software: you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
 
-   ExamGC is distributed in the hope that it will be useful, but
+   RappelDeC is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    General Public License for more details.
