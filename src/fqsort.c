70d89b1aeebfa825ea77c9d827cab0e5c775a6a6
Grégory Mounié <Gregory.Mounie@imag.fr>
2016-09-03 13:21:24 +0200

    fonction de trie

--- src/fqsort.c
+++ src/fqsort.c
@@ -1,11 +1,23 @@
 #include <complex.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
 
 /**
    Ajouter la fonction de comparaison du tri ici
 */
 
+int triarg(const void *a, const void *b) {
+	float complex const * ca = a;
+	float complex const * cb = b;
+
+	float tmp = (carg(*ca) - carg(*cb));
+	if (tmp < 0.0)
+		return -1;
+	if (tmp == 0.0)
+		return 0;
+	return 1;
+}
 
 int main(int argc, char **argv) {
 	(void) argc;
@@ -21,12 +33,13 @@ int main(int argc, char **argv) {
 	/**
 	   Mettre le code d'appel de qsort ici
 	*/
+	qsort(tableau, 100, sizeof(float complex), triarg);
 
 	/**
 	   Vérification du tri
 	*/
 	for(int i=0; i < 100-1; i++) {
-		assert(carg(tableau[i]) < carg(tableau[i+1]));
+		assert(carg(tableau[i]) <= carg(tableau[i+1]));
 	}
 	return 0;
 } 
