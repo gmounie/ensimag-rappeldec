544e306a5310f0766b3e2d1a00888afffa3460e6
Francois Broquedis <francois.broquedis@imag.fr>
2016-09-06 14:58:36 +0200

    Correction liste chainee.

--- src/listechainee.c
+++ src/listechainee.c
@@ -1,6 +1,6 @@
 /**
-   Copyright (C) 2016 by Gregory Mounie 
-   
+   Copyright (C) 2016 by Gregory Mounie
+
    This file is part of RappelDeC
 
    RappelDeC is free software: you can redistribute it and/or modify it
@@ -15,56 +15,117 @@
    General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <assert.h>
 
 struct elem {
-	int val;
-	struct elem* next;
+    int val;
+    struct elem *next;
 };
 
-void inversion_liste(struct elem **liste){
-	/**
-	   Votre code est à mettre ici !
-	*/
-	struct elem *tete = NULL;
-	while (*liste) {
-		struct elem *tmp = *liste;
-		*liste = (*liste)->next;
-		tmp->next = tete;
-		tete = tmp;
-	}
-	*liste = tete;
-};
+/* Crée une liste simplement chainée à partir des nb_elems éléments du
+ * tableau valeurs. */
+struct elem *creation_liste(int *valeurs, size_t nb_elems) {
+    assert(nb_elems != 0 && valeurs != NULL);
+    struct elem *tete = calloc(1, sizeof(struct elem));
+    tete->val = valeurs[0];
+    struct elem *courant = tete;
 
-const int TAILLE=100;
+    for (size_t i = 1; i < nb_elems; i++) {
+        courant->next = calloc(1, sizeof(struct elem));
+        courant->next->val = valeurs[i];
+        courant = courant->next;
+    }
 
-int main() {
-	struct elem *tabelem = malloc(sizeof(struct elem)*TAILLE);
-	assert(tabelem);
-	
-	for(int i=0; i < TAILLE; i++) {
-		tabelem[i] = (struct elem){i, & tabelem[i+1] };
-	}
-	tabelem[TAILLE-1].next = NULL;
+    return tete;
+}
 
-	struct elem *tete = & tabelem[0];
+/* Libère toute la mémoire associée à la liste passée en paramètre. */
+void destruction_liste(struct elem *liste) {
+    struct elem *courant = liste;
+    while (courant != NULL) {
+        struct elem *suivant = courant->next;
+        free(courant);
+        courant = suivant;
+    }
+}
 
-	inversion_liste(&tete);
-	assert(tete);
-	
-	struct elem *tmp= tete;
-	int idx= TAILLE-1;
-	for(;tmp; --idx, tmp=tmp->next) {
-		assert(tmp->val == idx);
-		assert(tmp ==  & tabelem[idx]);
-	}
+/* Affiche les éléments de la liste passée en paramètre sur la sortie
+ * standard. */
+void affichage_liste(struct elem *liste) {
+    for (struct elem *courant = liste; courant != NULL; courant = courant->next) {
+        printf("%u ", courant->val);
+    }
+    printf("\n");
+}
 
-	free(tabelem);
-	tabelem=NULL;
-	return 0;
+/* Inverse la liste simplement chainée passée en paramètre. Le
+ * paramètre liste contient l'adresse du pointeur sur la tête de liste
+ * à inverser. */
+void inversion_liste(struct elem **liste) {
+    /**
+       Votre code est à mettre ici !
+    */
+    if (liste == NULL || *liste == NULL) {
+        return;
+    }
+
+    struct elem *cur = *liste;
+    struct elem *next = cur->next;
+    cur->next = NULL;
+
+    while (next != NULL) {
+        struct elem *next_next = next->next;
+        next->next = cur;
+        cur = next;
+        next = next_next;
+    }
+
+    *liste = cur;
+}
+
+const int TAILLE = 100;
+
+int main(void)
+{
+    /* Test d'affichage d'une liste créée à la main. */
+    struct elem e1, e2, e3;
+    e1.val = 0;
+    e2.val = 1;
+    e3.val = 2;
+    e1.next = &e2;
+    e2.next = &e3;
+    e3.next = NULL;
+    affichage_liste(&e1);
+
+    int valeurs[TAILLE];
+    for (size_t i = 0; i < TAILLE; i++) {
+        valeurs[i] = i;
+    }
+    struct elem *tab_elem = creation_liste(valeurs, TAILLE);
+    assert(tab_elem != NULL);
+
+    printf("Liste créée:\n");
+    affichage_liste(tab_elem);
+    inversion_liste(&tab_elem);
+    assert(tab_elem != NULL);
+    printf("Liste inversée:\n");
+    affichage_liste(tab_elem);
+
+    struct elem *tmp;
+    int idx;
+    for (tmp = tab_elem, idx = TAILLE - 1; tmp != NULL; --idx, tmp = tmp->next) {
+        assert(tmp->val == idx);
+    }
+
+    /* Pour tester le bon fonctionnement de la fonction suivante, on
+     * lancera valgrind sur le programme pour vérifier qu'aucune fuite
+     * mémoire n'est présente. */
+    destruction_liste(tab_elem);
+
+    return EXIT_SUCCESS;
 }
