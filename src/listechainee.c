391c5b4717d1dacd59f88c29daf7e642a587c37b
Francois Broquedis <francois.broquedis@imag.fr>
2016-09-06 09:41:28 +0200

    exo listes : coding style.

--- src/listechainee.c
+++ src/listechainee.c
@@ -1,6 +1,6 @@
 /**
-   Copyright (C) 2016 by Gregory Mounie 
-   
+   Copyright (C) 2016 by Gregory Mounie
+
    This file is part of RappelDeC
 
    RappelDeC is free software: you can redistribute it and/or modify it
@@ -15,7 +15,7 @@
    General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <stdio.h>
@@ -23,40 +23,42 @@
 #include <assert.h>
 
 struct elem {
-	int val;
-	struct elem* next;
+    int val;
+    struct elem* next;
 };
 
-void inversion_liste(struct elem **liste){
-	/**
-	   Votre code est à mettre ici !
-	*/
+void inversion_liste(struct elem **liste) {
+    /**
+       Votre code est à mettre ici !
+    */
 };
 
 const int TAILLE=100;
 
-int main() {
-	struct elem *tabelem = malloc(sizeof(struct elem)*TAILLE);
-	assert(tabelem);
-	
-	for(int i=0; i < TAILLE; i++) {
-		tabelem[i] = (struct elem){i, & tabelem[i+1] };
-	}
-	tabelem[TAILLE-1].next = NULL;
+int main(void)
+{
+    struct elem *tabelem = malloc(sizeof(struct elem)*TAILLE);
+    assert(tabelem);
 
-	struct elem *tete = & tabelem[0];
+    for(int i = 0; i < TAILLE; i++) {
+        tabelem[i] = (struct elem){ i, &tabelem[i+1] };
+    }
+    tabelem[TAILLE-1].next = NULL;
 
-	inversion_liste(&tete);
-	assert(tete);
-	
-	struct elem *tmp= tete;
-	int idx= TAILLE-1;
-	for(;tmp; --idx, tmp=tmp->next) {
-		assert(tmp->val == idx);
-		assert(tmp ==  & tabelem[idx]);
-	}
+    struct elem *tete = &tabelem[0];
 
-	free(tabelem);
-	tabelem=NULL;
-	return 0;
+    inversion_liste(&tete);
+    assert(tete != NULL);
+
+    struct elem *tmp= tete;
+    int idx = TAILLE - 1;
+    for(;tmp; --idx, tmp=tmp->next) {
+        assert(tmp->val == idx);
+        assert(tmp ==  & tabelem[idx]);
+    }
+
+    free(tabelem);
+    tabelem = NULL;
+
+    return 0;
 }
