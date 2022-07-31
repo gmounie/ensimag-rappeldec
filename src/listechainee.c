1761d72610f4f7cdb3c1c79277b0b945473f4a3f
Grégory Mounié <Gregory.Mounie@imag.fr>
2022-07-31 12:06:02 +0200

    C11 array notation + clang format

--- src/listechainee.c
+++ src/listechainee.c
@@ -18,100 +18,108 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <stdlib.h>
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct elem {
-    long unsigned int val;
-    struct elem *next;
+  long unsigned int val;
+  struct elem *next;
 };
 
 /* Affiche les éléments de la liste passée en paramètre sur la sortie
  * standard. */
 void affichage_liste(struct elem *liste) {
-    /**
-       Votre code est à mettre ici !
-    */
+  /**
+     Votre code est à mettre ici !
+  */
+  (void)liste; // to remove
 }
 
 /* Crée une liste simplement chainée à partir des nb_elems éléments du
  * tableau valeurs. */
-struct elem *creation_liste(long unsigned int *valeurs, size_t nb_elems) {
-    /**
-       Votre code est à mettre ici !
-    */
-
-    return NULL;
+struct elem *creation_liste(size_t nb_elems,
+                            long unsigned int valeurs[nb_elems]) {
+  /**
+     Votre code est à mettre ici !
+  */
+  (void)nb_elems; // to remove
+  (void)valeurs;  // to remove
+  return NULL;
 }
 
-/* Libère toute la mémoire associée à la liste passée en paramètre. */
-void destruction_liste(struct elem *liste) {
-    /**
-       Votre code est à mettre ici !
-    */
+/**
+   Libère toute la mémoire associée à la liste passée en paramètre.
+
+   @param liste head list, pointer on first element, not NULL
+*/
+void destruction_liste(struct elem liste[static 1]) {
+  /**
+     Votre code est à mettre ici !
+  */
+  (void)liste; // to remove
 }
 
-
-/* Inverse la liste simplement chainée passée en paramètre. Le
+/**
+ * Inverse la liste simplement chainée passée en paramètre. Le
  * paramètre liste contient l'adresse du pointeur sur la tête de liste
- * à inverser. */
-void inversion_liste(struct elem **liste) {
-    /**
-       Votre code est à mettre ici !
-    */
+ * à inverser.
+ * @param liste head list address, not NULL
+ */
+void inversion_liste(struct elem *liste[static 1]) {
+  /**
+     Votre code est à mettre ici !
+  */
+  (void)liste; // to remove
 }
 
 const long unsigned int TAILLE = 100;
 
-int main(void)
-{
-    /* Test d'affichage d'une liste créée à la main. */
-    struct elem e1, e2, e3;
-    e1.val = 0;
-    e2.val = 1;
-    e3.val = 2;
-    e1.next = &e2;
-    e2.next = &e3;
-    e3.next = NULL;
-    affichage_liste(&e1);
+int main(void) {
+  /* Test d'affichage d'une liste créée à la main. */
+  struct elem e1, e2, e3;
+  e1.val = 0;
+  e2.val = 1;
+  e3.val = 2;
+  e1.next = &e2;
+  e2.next = &e3;
+  e3.next = NULL;
+  affichage_liste(&e1);
 
-    long unsigned int valeurs[TAILLE];
-    for (size_t i = 0; i < TAILLE; i++) {
-        valeurs[i] = i;
-    }
+  long unsigned int valeurs[TAILLE];
+  for (size_t i = 0; i < TAILLE; i++) {
+    valeurs[i] = i;
+  }
 
-    struct elem *tab_elem = creation_liste(valeurs, TAILLE);
-    assert(tab_elem != NULL);
+  struct elem *tab_elem = creation_liste(TAILLE, valeurs);
+  assert(tab_elem != NULL);
 
-    /* Test que la liste dans l'ordre */
-    struct elem *tmp;
-    long unsigned int idx;
-    for (tmp = tab_elem, idx = 0;
-	 tmp != NULL;
-	 idx++, tmp = tmp->next) {
-        assert(tmp->val == idx);
-    }
-    /* Test que la liste est complète (à la suite du test sur l'ordre) */
-    assert(idx == TAILLE);
+  /* Test que la liste dans l'ordre */
+  struct elem *tmp;
+  long unsigned int idx;
+  for (tmp = tab_elem, idx = 0; tmp != NULL; idx++, tmp = tmp->next) {
+    assert(tmp->val == idx);
+  }
+  /* Test que la liste est complète (à la suite du test sur l'ordre) */
+  assert(idx == TAILLE);
 
-    printf("Liste créée:\n");
-    affichage_liste(tab_elem);
-    inversion_liste(&tab_elem);
-    assert(tab_elem != NULL);
-    printf("Liste inversée:\n");
-    affichage_liste(tab_elem);
+  printf("Liste créée:\n");
+  affichage_liste(tab_elem);
+  inversion_liste(&tab_elem);
+  assert(tab_elem != NULL);
+  printf("Liste inversée:\n");
+  affichage_liste(tab_elem);
 
-    /* Vérifie que la liste est inversée et complète */
-    for (tmp = tab_elem, idx = TAILLE - 1; tmp != NULL; --idx, tmp = tmp->next) {
-        assert(tmp->val == idx);
-    }
-    assert(idx == (long unsigned int)(0UL - 1));
-    
-    /* Pour tester le bon fonctionnement de la fonction suivante, on
-     * lancera valgrind sur le programme pour vérifier qu'aucune fuite
-     * mémoire n'est présente. */
-    destruction_liste(tab_elem);
+  /* Vérifie que la liste est inversée et complète */
+  for (tmp = tab_elem, idx = TAILLE - 1; tmp != NULL; --idx, tmp = tmp->next) {
+    assert(tmp->val == idx);
+  }
+  assert(idx == (long unsigned int)(0UL - 1));
 
-    return EXIT_SUCCESS;
+  /* Pour tester le bon fonctionnement de la fonction suivante, on
+   * lancera valgrind sur le programme pour vérifier qu'aucune fuite
+   * mémoire n'est présente. */
+  destruction_liste(tab_elem);
+
+  return EXIT_SUCCESS;
 }
