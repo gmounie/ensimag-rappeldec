164963902c929567144579e97a58c3a4c2e51bb7
Grégory Mounié <Gregory.Mounie@imag.fr>
2022-07-31 12:28:45 +0200

    usage of X + clang format

--- src/binaires.c
+++ src/binaires.c
@@ -1,18 +1,19 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 static unsigned long long X = 123456ULL;
 
 unsigned char crand48(void) {
-    /* Insérez votre code ici. */
-    return 0;
+  /* Insérez votre code ici. */
+  (void)X; // to remove
+
+  return 0;
 }
 
-int main(void)
-{
-    printf("%hhu\n", crand48());
-    printf("%hhu\n", crand48());
-    printf("%hhu\n", crand48());
+int main(void) {
+  printf("%hhu\n", crand48());
+  printf("%hhu\n", crand48());
+  printf("%hhu\n", crand48());
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
